0f8239024bdeec14671c4a9b8c71db2690741790
Taylor Flatt <taylorjflatt@gmail.com>
2016-09-03 22:02:42 -0500

    Update backup.sh
    Added parameter support to the script.
    Added comments making the code (especially the functions) a bit more clear.
    Various bug fixes.

--- backup.sh
+++ backup.sh
@@ -1,46 +1,48 @@
 #!/bin/bash
 
-# Version 1.1
+# Version 1.2
 # Author: Taylor Flatt
 # Recursive backup script that will backup both regular files as well as 
 # directories. Meant to be run as a cron script.
 #
 # This version only supports backing up of a single file to a directory.
 #
-# Usage: backup
+# Usage: backup SOURCE DESTINATION -[d]
 
-###############################################################################
-# Modify these values to suit the specific backup operation.
-#
-# Uncomment the following line to include a date in the destination directory.
-date=`date "+-%Y-%m-%d"`
+function print_usage()
+{
+	echo "Usage: $0 SOURCE DESTINATION [-d]"
+}
 
-# The absolute path file or directory you wish to copy.
-source="/home/taylor/myDir"
-
-# The absolute path destination of the backup.
-destination="/home/taylor/backup"
-#
-# End Parameter modification
-###############################################################################
-
-# If the date is unset, then set to empty string.
-if [[ ! -z "${date}" ]]; then
-	destination="$destination$date"
+# Check arguments
+if [[ $# < 2 || $# > 3 ]]; then
+	print_usage
+	exit 1;
 fi
 
-# If copying a directory, move into it. Else, move into the dir of the file.
-#if [[ -d "$source" ]]; then
-#	cd "$source"
-#else
-#	cd "${source%/*}"
-#fi
+source=$1
+destination=$2
 
+# Assign a date value if required.
+if [[ $# -eq 3 ]]; then
+	if [[ $3 == "-d" ]]; then
+		date=`date "+-%Y-%m-%d"`
+		destination="$destination$date"
+	else
+		print_usage
+		exit 1;
+	fi
+fi
+
+# Function that will copy a file from one directory to another.
+# Parameter 1: A specific file that will be copied to another location.
+# Parameter 2: The destination directory of the file. (Backup location).
 function copy_file()
 {
 	local fileSource=$1
 	local fileDest=$2
 	
+	# Create the name of the new (backup) file.
 	newFile="$fileDest"/"${fileSource##*/}".bak
 	
 	# If the file already exists, don't copy it.
@@ -56,20 +58,32 @@ function copy_file()
 	fi
 }
 
+# Function that will copy the contents of a directory from one directory to another.
+# This recursive function will iterate through each sub-directory calling copy_file()
+# as needed for each file.
+# Parameter 1: The source directory of the file.
+# Parameter 2: The destination directory of the file. (Backup location).
 function copy_directory()
 {
 	local fileSource=$1
 	local fileDest=$2
 	
-	local absDestDir=$(realpath "$fileDest")
+	local absDestDir=$(realpath "$fileDest" 2> /dev/null)
+	
+	if [[ -z "$absDestDir" ]]; then
+		echo "Error getting the proper destination path."
+		exit 1;
+	fi
 	
 	# Check if the backup directory exists. If it doesn't, create it.
 	if [[ ! -d "$fileDest" ]]; then
-		if ! mkdir "$fileDest"; then
+		if ! mkdir "$fileDest" 2> /dev/null; then
+			echo "Error creating the directory: $fileDest"
 			exit 1;
 		fi
 	fi
 	
+	# Move to the proper directory if necessary.
 	cd "$fileSource"
 	
 	for file in .* *; do
