9d3643492eaa8b49e314699276895cdda13012b8
Taylor Flatt <taylorjflatt@gmail.com>
2016-09-14 00:38:34 -0500

    Update prune.sh
    Added support for long options.
    Various bug fixes.
    Added a help option with detailed instructions.
    Added better error reporting for the user.
    Removed the debug comments.
    Fixed the regex to make it simpler.

--- prune.sh
+++ prune.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Version 0.5
+# Version 1.0
 # Author: Taylor Flatt
 # A prune script that will remove files in a directory who have existed for longer than 
 # a specified number of days.
 #
-# -a: Automated flag. This will run the script without user interaction and will take 
+# -d, --directory: Directory in which files are subject to be pruned.
+# -n, --days: Files older than this number of days are subject to be pruned.
+# -a, --automated: Automated flag. This will run the script without user interaction and will take 
 # the safest option in deleting files. It leaves at least the newest backup file.
 #
 # Note: Sudoers and the root user cannot run this script. Limit the amount of possible damage.
-# Not to mention, there is no reason to run as an elevated user.
 #
 # Usage: prune -d DIRECTORY -n NUM_DAYS [-a]
 
@@ -19,15 +20,36 @@ if [[ $EUID -eq 0 ]] || [[ ! -z $SUDO_USER ]]; then
 	exit 1
 fi
 
+# Displays the usage information.
 function print_usage()
 {
 	echo "Usage: $0 -d DIRECTORY -n NUM_DAYS [-a]"
 }
 
+# Displays the help information to a user.
 function print_help()
 {
-	echo "This script removes files from a directory that are older than a supplied number of days."
-	echo "I'll fill the rest out later."
+	print_usage
+	echo "Prunes files in DIRECTORY older tha NUM_DAYS."
+	echo ""
+	
+	echo -e "\t -d DIRECTORY, --directory \t\t Directory whose contents are subject to prune."
+	echo -e "\t -n NUM_DAYS, --days \t\t Files older than this will be subject to prune."
+	echo -e "\t -a, --automated \t\t\t Runs without user input and will at least keep the most recent file."
+	echo ""
+	
+	echo "Notes:"
+	echo "-When -a is not supplied, user interaction is default. User interaction requires confirmation on each removal."
+	echo "-This script cannot be run as sudo or root user. This is to reduce as much undefined behavior as possible."
+	echo ""
+	
+	echo "Examples:"
+	echo -e "\t prune -d /opt/myBackup -n 20 \t Prunes files older than 20 days from the /opt/myBackup directory."
+	echo -e "\t prune -d /mnt/share/backup -n 160 -a \t Prunes files older than 180 days from the /mnt/share/backup directory without user input."
+	echo ""
+	echo ""
+	
+	echo "Full documentation and source code can be found at: <www.github.com/taylorflatt/> under the appropriate repository."
 }
 
 # Check arguments
@@ -43,37 +65,58 @@ automated=
 newestFile=
 numFiles=
 numFilesDeleted=0
+removedFileNames=()
+
+# Global error variables
+numFilesError=0
+fileErrorNames=()
 
 # Font colors for error/success messages.
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 END_COLOR=`tput sgr0`
 
-# Parse the arguments.
-while getopts ":d:n:ha" opt; do
-	case $opt in
-	d)
-		directory=$OPTARG
-		;;
-	n)
-		numDays=$OPTARG
-		;;
-	h)
-		print_help
-		exit 0
-		;;
-	a)
-		automated="TRUE"
-		;;
-	?)
-		print_usage
-		exit 1	
-		;;
+# Parse arguments
+while :; do
+	case $1 in
+		-h|-\?|--help)
+			print_help
+			exit 0
+			;;
+		-d|--directory)
+			echo "Setting directory"
+			directory=("${2-}")
+			shift
+			;;
+		-n|--days)
+			echo "Setting days"
+			numDays=("${2-}")
+			shift
+			;;
+		-a|--automated)
+			echo "Setting automated"
+			automated="TRUE"
+			;;
+		-?*)
+			echo "Error, invalid parameter $1"
+			exit 1
+			;;
+		--)
+			echo "End of parameters"
+			shift
+			break
+			;;
+		*)
+			echo "Breaking loop"
+			break
 	esac
+	
+	shift
 done
 
 # Make sure the inputs are not empty and assigned.
 if [[ -z $directory ]] || [[ -z $numDays ]]; then
+	echo "First"
 	print_usage
 	exit 1
 fi
@@ -93,15 +136,13 @@ if [[ ! $numDays =~ $expr ]]; then
 	exit 1
 fi
 
-echo "Directory: $directory"
-echo "Number of Days: $numDays"
-echo "Automated: $automated"
-echo "Newest File: $newestFile"
-echo "Number of files found: $numFiles"
-
 # Author: pjh
 # Print the newest file, if any, matching the given pattern
-# Example usage: newest_matching_file 'file*'
+# Parameter 1: A regex pattern that will be used to match files in the cwd.
+# 
+# Usage: newest_matching_file PATTERN
+# Example: newest_matching_file 'file*'
+#
 # WARNING: Files whose names begin with a dot will not be checked
 function newest_matching_file()
 {
@@ -134,8 +175,11 @@ function newest_matching_file()
 }
 
 # Prompt for the user to determine if a file is to be removed.
+#
+# Usage: prunte_file_prompt
+#
 # WARNING: This will also actually remove files if the choice is to remove a file.
-function remove_file_prompt()
+function prune_file_prompt()
 {
 	local rmChoice=
 	while [[ "$rmChoice" != "y" ]] && [[ "$rmChoice" != "n" ]]; do
@@ -144,7 +188,7 @@ function remove_file_prompt()
 		
 		case "$rmChoice" in
 			"y")
-				rm -r "$file"
+				rm -rf "$file"
 				((numFilesDeleted++))
 				;;
 			"n")
@@ -157,8 +201,11 @@ function remove_file_prompt()
 	done
 }
 
-# Removes all files except the newest file as determined by the 
-# newest_matching_file function.
+# Removes all files except the newest file as determined by the newest_matching_file function.
+# Parameter 1: The directory from which files will be removed.
+# 
+# Usage: keep_newest_file DIRECTORY
+#
 # Note: It will NOT remove the CWD.
 function keep_newest_file()
 {
@@ -185,15 +232,19 @@ function keep_newest_file()
 			if [[ "$file" == "$dir" ]]; then
 				echo "Root directory, we should skip."
 				echo "Skipping $dir..."
+			# Prune files (Interactive).
 			elif [[ -z $automated ]]; then
-				remove_file_prompt
+				prune_file_prompt
+			# Prune files (Non-interactive).
 			else
 				# Force the removal in case the file is write protected.
-				if rm -rf "$file"; then		# No interaction
+				if rm -rf "$file"; then
 					echo "Deleting file: $file"
 					((numFilesDeleted++))
 				else
 					echo "Error deleting $file..."
+					((numFilesError++))
+					fileErrorNames+=("$file")
 				fi
 			fi
 		fi
@@ -202,20 +253,22 @@ function keep_newest_file()
 	return 0
 }
 
-# Removes files depending on user input.
+# Prunes files depending on user input.
 # Parameter 1: The directory from which files will be removed.
 # Parameter 2: The number of days from the current date in which files prior 
 # to that date will be removed. 
 # Parameter 3: Whether or not the script should run in an automated fashion. 
 # This will suppress any user interaction and will only perform safe deletes.
 #
-# Usage: remove_files /home/user1/directory7/backupDir 20 
-# Usage: remove_files /home/user1/directory7/backupDir 20 TRUE
+# Usage: prune_file DIRECTORY DAYS [AUTOMATED]
+#
+# Example: prune_files /home/user1/directory7/backupDir 20 
+# Example: prune_files /home/user1/directory7/backupDir 20 TRUE
 #
 # Note: ANY input for a third parameter will be read as an automated task. Typically 
 # passing in the value set for $automated in the main script parameter works just as well.
 # Note: In no case will it remove the CWD.
-function remove_files()
+function prune_files()
 {
 	if [[ $# < 2 ]] || [[ $# > 3 ]]; then
 		echo "Usage: $0 DIRECTORY DAYS [AUTOMATED]"
@@ -231,7 +284,6 @@ function remove_files()
 	
 	# Make sure the input is a valid directory.
 	if [[ ! -d "$dir" ]]; then
-		echo "(DEBUG) Remove_Files: $dir is NOT a directory."
 		echo "${RED}DIRECTORY must be a real and accessible directory.${END_COLOR}"
 		exit 1
 	fi
@@ -240,8 +292,6 @@ function remove_files()
 	if ! cd "$dir"; then
 		echo "${RED}Error changing directories to $dir ${END_COLOR}"
 		return 1
-	else
-		echo "(DEBUG) Remove_Files: Successfully moved to $dir"
 	fi
 	
 	# This way of processing is a bit more costly but allows a safety check.
@@ -264,30 +314,14 @@ function remove_files()
 		((totalFiles++))
 	done
 	
-	echo "================================="
-	echo "Remove_Files Function Parameters"
-	echo "================================="
-	echo "Directory: $dir"
-	echo "Days: $days"
-	echo "Automated: $automated"
-	echo "Date: $date"
-	echo "TotalFiles: $totalFiles"
-	echo "NumFiles: $numFiles"
-	echo "================================="
-	
-	echo "(DEBUG) Remove_Files: Passed the for-loop."
-	
 	# If numFiles=totalFiles, then this determines if all files in dir will be removed.
 	local deleteAll=
 	
-	##########################
-	# Problem Cases
-	##########################
-	# Case in which the number of files to delete is more than the files in the directory.
+	# Case: Number of files to delete outnumbers the files in the directory. (Did we catch . or ..?)
 	if [[ numFiles > totalFiles ]]; then
 		echo "{RED}Cannot delete more files than actually exist. Something went wrong.{END_COLOR}."
 		exit 1
-	# Case in which all files could be removed.
+	# Case: All files in the directory are marked for pruning. Check with the user if interactive, otherwise keep newest file.
 	elif [[ numFiles -eq totalFiles ]]; then
 		if [[ -z "$automated" ]]; then
 			delPrompt=
@@ -318,13 +352,11 @@ function remove_files()
 		fi
 	fi
 	
-	##########################
-	# Remove Files
-	##########################
-	if [[ -z $automated ]]; then
+	# Remove files (Interactive)
+	if [[ -z "$automated" ]]; then
 		if [[ "$deleteAll" == "TRUE" ]]; then
 			for file in *; do
-				remove_file_prompt
+				prune_file_prompt
 			done
 		elif [[ "$deleteAll" == "FALSE" ]]; then
 			keep_newest_file "$dir"
@@ -337,24 +369,34 @@ function remove_files()
 						echo "Root directory, we should skip."
 						echo "Skipping $dir..."
 					else
-						remove_file_prompt
+						prune_file_prompt
 					fi
 				fi
 			done
 		fi
+	# Remove files (Non-interactive)
 	else
 		keep_newest_file "$dir"
 	fi
 }
 
-# Run the prune.
+# Prune (Non-interactive).
 if [[ -z "$automated" ]]; then
-	remove_files "$directory" "$numDays"
+	prune_files "$directory" "$numDays"
+# Prune (Interactive).
 else
-	remove_files "$directory" "$numDays" "$automated"
+	prune_files "$directory" "$numDays" "$automated"
 fi
 
 echo "${GREEN}Successfully removed $numFilesDeleted files from $directory! ${END_COLOR}"
+
+# If there is an error (or more), indicate the files that failed to be removed.
+if [[ $numFilesError -ne 0 ]]; then
+	echo ""
+	echo ""
+	echo "${RED} The following $numFilesError files failed to delete:{END_COLOR}"
+	printf 'Removed %s...\n' "${fileErrorNames[@]}"
+fi
 
 exit 0
 #EOF
