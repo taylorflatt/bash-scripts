0dc8f4187fb33d5d54f095cb0056ce9a50910dff
Taylor Flatt <taylorjflatt@gmail.com>
2017-01-13 12:42:50 -0600

    Update create_vbvm.sh

--- VirtualBox/create_vbvm.sh
+++ VirtualBox/create_vbvm.sh
@@ -6,7 +6,7 @@
 #
 # Note: This uses a readarray which requires Bash 4.0.
 #
-# Usage: ./create_vbvm.sh TEMPLATE
+# Usage: ./create_virtualbox.sh TEMPLATE
 
 print_usage()
 {
@@ -23,12 +23,19 @@ fi
 # Local variables.
 template=$1
 vmName=
-value=
+os=
+ram=
 
 # Gets the text to the right of the input delimited by a :.
 get_value()
 {
 	local result=$(echo "$1" | cut -d ':' -f2)
+	
+	# Removes in-line comments (if any).
+	if [[ "$result" = *"#"* ]]; then
+		result=$(echo "$result" | cut -d '#' -f1)
+	fi
+	
 	echo "$result"
 }
 
@@ -36,37 +43,47 @@ readarray -t lines < "$template"
 
 for line in "${lines[@]}"; do
 	case "$line" in
+		# Line is a comment.
 		"#"*)
-			echo "This is a commented line"
 			;;
 		# Process VM Name.
 		"NAME:"*)
 			# Get the current list of VMs and make sure the new one isn't already on it.
 			currentVms=$(VBoxManage list vms)
 			vmName=$(get_value "$line")
-			if [[ $line = *"$vmName"* ]]; then
+			if [[ $currentVms = *"$vmName"* ]]; then
 				echo "A VM with the name $vmName has already been created. You must first delete it."
 				exit 1
 			fi
 			;;
 		# Process OS Type.
 		"OS:"*)
-			value=$(get_value "$line")
-			VBoxManage createvm --name $vmName --ostype "$value" --register
+			os=$(get_value "$line")
 			;;
 		# Process RAM.
 		"RAM:"*)
-			value=$(get_value "$line")
-			VBoxManage modifyvm $vmName --memory $value
+			ram=$(get_value "$line")
+			# Make sure the RAM value is an integer.
+			if [ "$ram" -ne "$ram" ] 2> /dev/null; then
+				echo "The RAM value should be an integer representing the number of MB of RAM for the VM."
+				exit 1
+			fi
 			;;
-		# Catch any error.
+		# Catch any error/syntax fault.
 		*)
 			echo "Unrecognized syntax: $line"
-			if [[ $vmName != "" ]]; then
-				VBoxManage unregistervm $vmName
-			fi
+			exit 1
 			;;
 	esac
 done
+
+# Create and register the VM.
+#VBoxManage createvm --name $vmName --ostype "$os" --register
+
+# Modify its properties.
+#VBoxManage modifyvm $vmName --memory "$ram"
+
+# If any problem occurs after creating the VM, we should probably unregister it or give the user 
+# the option to do so. The command is: VBoxManage unregistervm $vmName
 
 exit 0
